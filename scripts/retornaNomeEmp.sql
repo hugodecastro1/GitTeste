ed7c57f69ff597dfbd23ed963db961ba3a095e7a
Hugo Reboucas <hugodecastro1@gmail.com>
2020-07-06 08:52:56 -0300

    criado função retornaNomeEmp...

---
+++ scripts/retornaNomeEmp.sql
@@ -0,0 +1,12 @@
+﻿create or replace function retornaNomeEmp(nid int)
+returns text
+as
+$$
+declare
+	cnome varchar(100);
+begin
+
+	execute 'select nome from empregado where id = '||nid into cnome; 
+	return cnome;
+end;
+$$ language plpgsql;
